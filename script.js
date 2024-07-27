c250ae48904b05a75026fbd008ca27b68b95d148
metasploit meta <baretoneee@gmail.com>
2024-07-27 11:50:10 +0530

    all

---
+++ script.js
@@ -0,0 +1,43 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//    var myDiv  = document.getElementById('myDiv');
+//    myDiv.style.margin = '20px';
+//    myDiv.style.padding = '10px';
+//    myDiv.style.fontSize = '18px';
+//    myDiv.style.fontWeight = 'bold';
+//    myDiv.style.height = '200px';
+//    myDiv.style.width = '300px';
+//    myDiv.style.background = 'yellow';
+//    myDiv.style.display = 'flex';
+//    myDiv.style.justifyContent = 'center'
+//    myDiv.style.alignItems = 'center'
+// });
+
+// let btn = document.querySelector("#btn"); // for selecting any of element
+// let myDiv = document.getElementById("myDiv");
+// let toggle ;
+// btn.addEventListener("click", function () { //
+//     console.log(toggle + " " + "outer block if else");
+//   if(toggle){
+//     myDiv.style.color = "red";
+//     toggle =false;
+//     console.log(toggle + " " + "if");
+//   }else{
+//     myDiv.style.color = "black";
+//     toggle = true;
+//     console.log(toggle+ " " + "else");
+//   }
+// });
+
+let div = document.querySelector("#myDiv");
+document.addEventListener("DOMContentLoaded",function(){
+   myDiv.style.margin = '20px';
+   myDiv.style.padding = '10px';
+   myDiv.style.fontSize = '18px';
+   myDiv.style.fontWeight = 'bold';
+   myDiv.style.height = '200px';
+   myDiv.style.width = '300px';
+   myDiv.style.background = 'yellow';
+   myDiv.style.display = 'flex';
+   myDiv.style.justifyContent = 'center'
+   myDiv.style.alignItems = 'center'
+})
